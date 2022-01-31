8d75232fb283accb6127bbbdb96235632ca3570c
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-31 17:07:15 -0500

    add current operand, previous operand, operation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { useReducer } from 'react';
 import './App.css';
 
 function App() {
+   const [{ currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
+
+
   return (
     <div className="calculator-grid">
     <div className="output">
-      <div className="previous-operand"></div>
-      <div className="current-operand"></div>
+      <div className="previous-operand">{previousOperand} {operation}</div>
+      <div className="current-operand">{currentOperand}</div>
     </div>
     <button className="span-two">AC</button>
     <button>DEL</button>
